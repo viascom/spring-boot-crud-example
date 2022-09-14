3b4d41e779edf740014dc7c1b7184c5afb38ea59
Nikola Stanković <nikola.stankovic@viascom.ch>
2022-09-14 10:19:02 +0200

    chore: clean up

--- src/main/java/io/viascom/springbootcrudexample/CrudExampleApplication.java
+++ src/main/java/io/viascom/springbootcrudexample/CrudExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrudExampleApplication {
+public class CrudExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudExampleApplication.class, args);
+		SpringApplication.run(CrudExampleApplication.class, args);
 	}
 
 }
