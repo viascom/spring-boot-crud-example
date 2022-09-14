71c57d0f42e21bb92e986ae07406ef1c86f89e65
Nikola Stanković <nikola.stankovic@viascom.ch>
2022-09-14 10:05:22 +0200

    feat: add base structure

---
+++ src/test/java/io/botscripter/springbootcrudexample/SpringBootCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.botscripter.springbootcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
