ab047541fcbfe24650c60a1210d115858216d968
Nikola Stanković <nikola.stankovic@viascom.ch>
2022-09-19 20:34:00 +0200

    chore: add password hint

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,4 +9,4 @@ VALUES ('413e2297-b84b-42ef-97ed-16a8a9d1d671', 'APEX Legends', '4be5f5bf-8eb5-4
        ('3c13c533-fbac-4881-b94d-f95cb2ef16c8', 'League of Legends', '692fa28c-fafd-442b-8022-4ed792995053');
 
 INSERT INTO MEMBER (id, username, password_hash, is_admin)
-VALUES ('9135f12e-1b66-4ee6-bbae-df37303cc154', 'admin', '$2a$10$aDD6I9Ej5.W8busvlsdPx.JvMWyJX8cOeOfVb.3q73KH2swww/N9C', true);
+VALUES ('9135f12e-1b66-4ee6-bbae-df37303cc154', 'admin', '$2a$10$aDD6I9Ej5.W8busvlsdPx.JvMWyJX8cOeOfVb.3q73KH2swww/N9C', true); -- Password: password1234
